e0ce21417d76738232a6ed8ed7666d02684293df
akshay619-dev <akshay.patil61@gmail.com>
2024-07-07 02:13:23 +0530

    Added validation in backend apis

--- backend/checkout/views.py
+++ backend/checkout/views.py
@@ -9,9 +9,18 @@ class AddItemView(APIView):
     def post(self, request):
         global checkout  
         checkout = Checkout()
-        items_string = request.data.get('items', '')  
+        items_string = request.data.get('items', []) 
+        if not items_string:
+            return Response({'message': 'No items provided'}, status=status.HTTP_400_BAD_REQUEST)
+        
         items = list(items_string)  
         for item in items:
+            if not isinstance(item, str):
+                return Response({'message': 'Invalid item type'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if checkout.validate_item(item):
+                return Response({'message': 'Item not found in products'}, status=status.HTTP_400_BAD_REQUEST)
+            
             checkout.add_item(item)
         return Response({"message": "Items added successfully"}, status=status.HTTP_200_OK)
 
